395d91b3ab3efeae2f2da4e87946682a0a0883bc
Aikel <aikelen.030904@gmail.com>
2024-11-13 20:27:55 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //1
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName+firstName+middleName;
+        System.out.println(fullName);
 
-        System.out.println("Hello world!");
+        //2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — "+fullName.toUpperCase());
+        //3
+        String fullNameTrue = "Иванов Семён Семёнович";
+        fullNameTrue= fullNameTrue.replace("ё", "е");
+        System.out.println(fullNameTrue);
     }
 }
